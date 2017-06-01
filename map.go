10198f66e8267031b2541282090baa6ec42383fe
田兵 <bing.tian@ele.me>
2017-06-01 17:35:04 +0800

    zk client

--- map.go
+++ map.go
@@ -2,13 +2,54 @@ package main
 
 import (
 	"fmt"
-	"goio/util"
+	"time"
+
+	"github.com/samuel/go-zookeeper/zk"
 )
 
 func main() {
-	var ctrie *util.Ctrie = util.New(nil)
-	ctrie.Insert([]byte("key"), 1)
+	var (
+		c     *zk.Conn
+		paths []string
+		err   error
+	)
+	c, _, err = zk.Connect([]string{"172.16.6.77"}, 500*time.Millisecond)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("connect success")
+	paths, _, err = c.Children("/barrage")
+	/*	paths, _, err = c.Children("/barrage/server")
 
-	a, _ := ctrie.Remove([]byte("key"))
-	fmt.Println(a)
+		fmt.Println(paths)
+		if err != nil {
+			fmt.Println(err)
+		}
+		sort.Strings(paths)
+		fmt.Println(paths)
+		data, _, err := c.Get("/barrage/server/" + paths[0])
+		if err != nil {
+			fmt.Println(err)
+		}
+	*/
+	//	is_exist, _, err := c.Exists("/barrage/master")
+	//if err != nil {
+	//fmt.Println("Exists", err)
+	//}
+	/*	if !is_exist {
+		_, err := c.Create("/barrage/master", data, 0, zk.WorldACL(zk.PermAll))
+		if err != nil {
+			fmt.Println("Create", err)
+		}
+	} else {*/
+	//	_, stat, err := c.Get("/barrage/master")
+	//_, err = c.Set("/barrage/master", data, stat.Version)
+	data, _, err := c.Get("/barrage/" + paths[0])
+	if err != nil {
+		fmt.Println("Get", err)
+	}
+	fmt.Println(string(data))
+	//}
+
+	//	time.Sleep(1 * time.Hour)
 }
