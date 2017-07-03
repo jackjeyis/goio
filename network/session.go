63d0cb03487dd91613513722e676d3cc61cad40a
田兵 <bing.tian@ele.me>
2017-07-03 19:01:36 +0800

    ctrie to syncmap

--- network/session.go
+++ network/session.go
@@ -17,8 +17,8 @@ func init() {
 	once.Do(func() {
 		//b := NewBucket(256)
 		s = &Session{
-			ctrie: util.New(nil),
-			room:  util.New(nil),
+			ctrie: &util.Map{},
+			room:  &util.Map{},
 			size:  1024,
 		}
 		for i := uint64(0); i < s.size; i++ {
@@ -50,7 +50,7 @@ func NewBucket(size int) *Bucket {
 	}
 
 	for i := 0; i < size; i++ {
-		b.buckets[i] = &Session{ctrie: util.New(nil), room: util.New(nil)}
+		b.buckets[i] = &Session{ctrie: &util.Map{}, room: &util.Map{}}
 	}
 
 	return b
@@ -62,36 +62,45 @@ func (b *Bucket) HashSession(key string) *Session {
 }
 
 type Session struct {
-	ctrie *util.Ctrie
-	room  *util.Ctrie
+	ctrie *util.Map
+	room  *util.Map
 	queue [1024]chan *protocol.Barrage
 	size  uint64
 }
 
 type Room struct {
-	chs  *util.Ctrie
+	chs  *util.Map
 	rid  string
-	host *util.Ctrie
+	host *util.Map
 }
 
 func NewRoom(id string) *Room {
 	return &Room{
-		chs:  util.New(nil),
+		chs:  &util.Map{},
 		rid:  id,
-		host: util.New(nil),
+		host: &util.Map{},
 	}
 }
 
 func (r *Room) addChan(cid, uid string, ch msg.Channel) {
-	c, _ := r.chs.Lookup([]byte(uid))
+	c, _ := r.chs.Load(uid)
 	if c == nil {
-		c = util.New(nil)
-		r.chs.Insert([]byte(uid), c)
+		c = &util.Map{}
+		r.chs.Store(uid, c)
 	}
-	c.(*util.Ctrie).Insert([]byte(cid), ch)
+	c.(*util.Map).Store(cid, ch)
 }
 
-func (r *Room) getMember() (chans []msg.Channel, uids []string, rcount int) {
+func (r *Room) getMember() (uids []string, rcount int) {
+	r.chs.Range(func(key, value interface{}) bool {
+		uids = append(uids, key.(string))
+		rcount += 1
+		return true
+	})
+	return
+}
+
+/*func (r *Room) getMember() (chans []msg.Channel, uids []string, rcount int) {
 	for entry := range r.chs.Iterator(nil) {
 		for item := range entry.Value.(*util.Ctrie).Iterator(nil) {
 			chans = append(chans, item.Value.(msg.Channel))
@@ -100,9 +109,27 @@ func (r *Room) getMember() (chans []msg.Channel, uids []string, rcount int) {
 		rcount += 1
 	}
 	return
-}
+}*/
 
 func (r *Room) pushMsg(barrage protocol.Barrage) {
+	r.chs.Range(func(key, value interface{}) bool {
+		value.(*util.Map).Range(func(key, value interface{}) bool {
+			c := value.(msg.Channel)
+			if c == nil || c.GetAttr("cid") == barrage.Channel().GetAttr("cid") {
+				return true
+			}
+
+			barrage.Ver = 1
+			barrage.Op = 5
+			barrage.SetChannel(c)
+			c.EncodeMessage(&barrage)
+			return true
+		})
+		return true
+	})
+}
+
+/*func (r *Room) pushMsg(barrage protocol.Barrage) {
 	for entry := range r.chs.Iterator(nil) {
 		for item := range entry.Value.(*util.Ctrie).Iterator(nil) {
 			c := item.Value.(msg.Channel)
@@ -115,54 +142,51 @@ func (r *Room) pushMsg(barrage protocol.Barrage) {
 			c.EncodeMessage(&barrage)
 		}
 	}
-}
+}*/
 
 func (r *Room) deleteChan(cid, uid string) {
-	c, _ := r.chs.Lookup([]byte(uid))
+	c, _ := r.chs.Load(uid)
 	if c != nil {
-		ch := c.(*util.Ctrie)
-		ch.Remove([]byte(cid))
+		ch := c.(*util.Map)
+		ch.Delete(cid)
 		if ch.Size() == 0 {
-			r.chs.Remove([]byte(uid))
+			r.chs.Delete(uid)
 		}
 	}
 }
 
 func (s *Session) insert(cid string, ch msg.Channel, host int) {
 	var room *Room
-	s.ctrie.Insert([]byte(cid), ch)
+	s.ctrie.Store(cid, ch)
 	rid := ch.GetAttr("rid")
-	r, _ := s.room.Lookup([]byte(rid))
+	r, _ := s.room.Load(rid)
 	if r == nil {
 		room = NewRoom(rid)
-		s.room.Insert([]byte(rid), room)
+		s.room.Store(rid, room)
 	} else {
 		room = r.(*Room)
 	}
 	if host != 2 {
-		room.host.Insert([]byte(cid), ch)
+		room.host.Store(cid, ch)
 	}
 	room.addChan(cid, ch.GetAttr("uid"), ch)
 }
 
 func (s *Session) delete(cid, uid, rid string) {
-	ch, _ := s.ctrie.Remove([]byte(cid))
-	if ch == nil {
-		return
-	}
+	s.ctrie.Delete(cid)
 	r := s.roomer(rid)
 	if r != nil {
 		if r.chs.Size() == 0 {
-			s.room.Remove([]byte(rid))
+			s.room.Delete(rid)
 		} else {
 			r.deleteChan(cid, uid)
-			r.host.Remove([]byte(cid))
+			r.host.Delete(cid)
 		}
 	}
 }
 
 func (s *Session) find(cid string) msg.Channel {
-	ch, ok := s.ctrie.Lookup([]byte(cid))
+	ch, ok := s.ctrie.Load(cid)
 	if ok {
 		return ch.(msg.Channel)
 	}
@@ -170,7 +194,7 @@ func (s *Session) find(cid string) msg.Channel {
 }
 
 func (s *Session) roomer(rid string) *Room {
-	r, ok := s.room.Lookup([]byte(rid))
+	r, ok := s.room.Load(rid)
 	if !ok {
 		return nil
 	}
@@ -189,21 +213,21 @@ func GetSession(cid string) msg.Channel {
 	return s.find(cid)
 }
 
-func GetRoomSession(rid string) []msg.Channel {
+/*func GetRoomSession(rid string) []msg.Channel {
 	r := s.roomer(rid)
 	if r == nil {
 		return nil
 	}
 	chans, _, _ := r.getMember()
 	return chans
-}
+}*/
 
 func GetRoomStatus(rid string) (int, []string) {
 	r := s.roomer(rid)
 	if r == nil {
 		return 0, nil
 	}
-	_, uids, count := r.getMember()
+	uids, count := r.getMember()
 	return count, uids
 }
 
@@ -232,11 +256,12 @@ func NotifyHost(rid, cid, uid string, code int8) {
 	ch.SetAttr("cid", cid)
 	barrage.SetChannel(ch)
 	if code == 0 {
-		for item := range r.host.Iterator(nil) {
-			ch := item.Value.(msg.Channel)
+		r.host.Range(func(key, value interface{}) bool {
+			ch := value.(msg.Channel)
 			barrage.SetChannel(ch)
-			barrage.Channel().EncodeMessage(&barrage)
-		}
+			ch.EncodeMessage(&barrage)
+			return true
+		})
 	} else {
 		BroadcastRoom(barrage, false)
 	}
